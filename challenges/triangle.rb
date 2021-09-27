a4a65191ba1cea3f8663377eda95be239de70764
Leena <leena@pop-os.localdomain>
2021-09-27 12:43:02 -0400

    triangle challenge

---
+++ challenges/triangle.rb
@@ -0,0 +1,41 @@
+=begin
+Write a program to determine whether a triangle is equilateral, isosceles, or scalene.
+
+An equilateral triangle has all three sides the same length.
+
+An isosceles triangle has exactly two sides of the same length.
+
+A scalene triangle has all sides of different lengths.
+
+Note: For a shape to be a triangle at all, all sides must be of length > 0, and the sum of the lengths of any two sides must be greater than the length of the third side.
+
+
+=end
+
+class Triangle
+  attr_reader :sides
+
+  def initialize(side1, side2, side3)
+    @sides = [side1, side2, side3]
+    raise ArgumentError.new "Invalid triangle lengths" unless valid?
+  end
+
+  def kind
+    if sides.uniq.size == 1
+      'equilateral'
+    elsif sides.uniq.size == 2
+      'isosceles'
+    else
+      'scalene'
+    end
+  end
+
+  private
+
+  def valid?
+    sides.all? { |side| side > 0} &&
+    sides[0] + sides[1] > sides[2] &&
+    sides[1] + sides[2] > sides[0] &&
+    sides[0] + sides[2] > sides[1]
+  end
+end
